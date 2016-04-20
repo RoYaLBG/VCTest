c8a77e3b8e27e1e97ef8345587748dca6c8e6b55
RoYaLBG <royal4o@gmail.com>
2016-04-20 18:21:19 +0300

    tests

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by RoYaL on 4/20/2016.
+ */
+public class FirstTest {
+
+    @Test
+    public void test1() {
+        Assert.fail();
+    }
+}
